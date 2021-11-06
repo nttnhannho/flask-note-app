fd80616c81f3e3e7a12e9e0c775e0db2b4a97583
nttnhannho <makelifeperfect@gmail.com>
2021-11-06 22:59:49 +0700

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# flask-note-app
+In this project, I developed an web app used to manage the user notes.
+User can sign up an new account, login and create their own notes.
---
+++ website/views.py
@@ -0,0 +1,37 @@
+import json
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from . import db
+from .models import Note
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/", methods=["GET", "POST"])
+@login_required
+def home():
+    if request.method == "POST":
+        note = request.form.get("note")
+        if len(note) < 1:
+            flash("Note is too short", category="error")
+        else:
+            new_note = Note()
+            new_note.data = note
+            new_note.user_id = current_user.id
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added!", category="success")
+    return render_template("views/home.html", user=current_user)
+
+
+@views.route("/delete-note", methods=["POST"])
+def delete_note():
+    data = json.loads(request.data)
+    note_id = data['note_id']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
